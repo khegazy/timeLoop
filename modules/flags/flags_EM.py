56f0e22f50074e33e5fb38fa915ae5216dde0590
khegazy <kareemhegz@gmail.com>
2020-05-18 12:19:59 -0700

    Resolved merge conflict.

--- modules/flags/flags_EM.py
+++ modules/flags/flags_EM.py
@@ -15,22 +15,17 @@ def get_flags():
   FLAGS = flags.FLAGS
 
   flags.DEFINE_string(
-<<<<<<< HEAD
       "experiment", "time_dep_weights",
       "Name of the experiment.")
 
   flags.DEFINE_string(
-=======
->>>>>>> 3c797c16f759928cf2cb6f1adeaeee7f33375f50
+
       "molecule", "CHO",
       "Name of the molecule under investigation.")
 
   flags.DEFINE_integer(
-<<<<<<< HEAD
       "Nsteps", 802,
-=======
-      "Nsteps", 50000,
->>>>>>> 3c797c16f759928cf2cb6f1adeaeee7f33375f50
+
       "Number of fitting steps to take.")
 
   flags.DEFINE_string(
@@ -38,11 +33,7 @@ def get_flags():
       "Optimizer for fitting.")
 
   flags.DEFINE_float(
-<<<<<<< HEAD
       "SGD_LR", 1e-6,
-=======
-      "SGD_LR", 1e-3,
->>>>>>> 3c797c16f759928cf2cb6f1adeaeee7f33375f50
       "Learning rate for SGD.")
 
   flags.DEFINE_string(
@@ -50,7 +41,6 @@ def get_flags():
       "Indicate which loss to use for diffraction and PC.")
 
   flags.DEFINE_float(
-<<<<<<< HEAD
       "scale_diffraction_loss", 1e-4,
       "Scaling hyperparameter for the diffraction loss contribution.")
 
@@ -60,17 +50,6 @@ def get_flags():
 
   flags.DEFINE_float(
       "scale_init_zero_loss", 1e3,
-=======
-      "scale_diffraction_loss", 1e1,#1e-4,
-      "Scaling hyperparameter for the diffraction loss contribution.")
-
-  flags.DEFINE_float(
-      "scale_pairCorr_loss", 6e3,
-      "Scaling hyperparameter for the pair correlation loss contribution.")
-
-  flags.DEFINE_float(
-      "scale_init_zero_loss", 1e2,
->>>>>>> 3c797c16f759928cf2cb6f1adeaeee7f33375f50
       "Scaling hyperparameter for the init zero loss contribution.")
 
   flags.DEFINE_float(
@@ -81,7 +60,6 @@ def get_flags():
       "momentum_scale", 0.05,
       "Characteristic momentum in A*AU/fs for the momentum loss.")
 
-<<<<<<< HEAD
   flags.DEFINE_float(
       "scale_force_loss", 3e-4,
       "Scaling hyperparameter for the force loss contribution.")
@@ -94,18 +72,12 @@ def get_flags():
       "scale_expect_pos_loss", 1e4,
       "Characteristic momentum in A*AU/fs**2 for the momentum loss.")
 
-=======
->>>>>>> 3c797c16f759928cf2cb6f1adeaeee7f33375f50
   flags.DEFINE_boolean(
       "debugging", False,
       "Debugging option that defines what debugging study to do.")
 
   flags.DEFINE_string(
-<<<<<<< HEAD
       "debugging_opt", None,# "calc_grad_mags",#"init_to_noisy_data,calc_grad_mags",
-=======
-      "debugging_opt", "calc_grad_mags",#"init_to_noisy_data,calc_grad_mags",
->>>>>>> 3c797c16f759928cf2cb6f1adeaeee7f33375f50
       "Debugging option that defines what debugging study to do.")
 
   return FLAGS
